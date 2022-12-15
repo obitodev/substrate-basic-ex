0ea0a291ce750fdffac5f795041d2f55a6caf452
obitodev <anhhaiquynhon123@gmail.com>
2022-12-15 18:10:54 +0700

    testing

--- pallets/kitties/src/mock.rs
+++ pallets/kitties/src/mock.rs
@@ -1,6 +1,8 @@
 use crate as pallet_kitties;
-use frame_support::traits::{ConstU16, ConstU64};
+use frame_support::parameter_types;
+use frame_support::traits::{ConstU16, ConstU32, ConstU64};
 use frame_system as system;
+use sp_consensus_aura::sr25519::AuthorityId as AuraId;
 use sp_core::H256;
 use sp_runtime::{
 	testing::Header,
@@ -10,6 +12,11 @@ use sp_runtime::{
 type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
 type Block = frame_system::mocking::MockBlock<Test>;
 
+parameter_types! {
+	// kitties
+	pub const KittyOwnedLimit: u32 = 3;
+}
+
 // Configure a mock runtime to test the pallet.
 frame_support::construct_runtime!(
 	pub enum Test where
@@ -18,6 +25,9 @@ frame_support::construct_runtime!(
 		UncheckedExtrinsic = UncheckedExtrinsic,
 	{
 		System: frame_system,
+		RandomnessCollectiveFlip: pallet_randomness_collective_flip,
+		Timestamp: pallet_timestamp,
+		Aura: pallet_aura,
 		KittiesModule: pallet_kitties,
 	}
 );
@@ -49,8 +59,36 @@ impl system::Config for Test {
 	type MaxConsumers = frame_support::traits::ConstU32<16>;
 }
 
+impl pallet_randomness_collective_flip::Config for Test {}
+
+impl pallet_aura::Config for Test {
+	type AuthorityId = AuraId;
+	type DisabledValidators = ();
+	type MaxAuthorities = ConstU32<32>;
+}
+
+/// An index to a block.
+/// Change this to adjust the block time.
+pub const MILLISECS_PER_BLOCK: u64 = 6000;
+
+// NOTE: Currently it is not possible to change the slot duration after the chain has started.
+//       Attempting to do so will brick block production.
+pub const SLOT_DURATION: u64 = MILLISECS_PER_BLOCK;
+
+// Time is measured by number of blocks.
+impl pallet_timestamp::Config for Test {
+	/// A timestamp: milliseconds since the unix epoch.
+	type Moment = u64;
+	type OnTimestampSet = Aura;
+	type MinimumPeriod = ConstU64<{ SLOT_DURATION / 2 }>;
+	type WeightInfo = ();
+}
+
 impl pallet_kitties::Config for Test {
 	type Event = Event;
+	type Time = Timestamp;
+	type KittyOwnedLimit = KittyOwnedLimit;
+	type KittyRandomDna = RandomnessCollectiveFlip;
 }
 
 // Build genesis storage according to the mock runtime.
--- pallets/kitties/src/tests.rs
+++ pallets/kitties/src/tests.rs
@@ -1,13 +1,53 @@
-use crate::{mock::*, Error};
-use frame_support::{assert_noop, assert_ok};
+use crate::mock::*;
+use frame_support::assert_ok;
 
 #[test]
-fn it_works_for_default_value() {
+fn test_create_kitty() {
 	new_test_ext().execute_with(|| {
-		// Dispatch a signed extrinsic.
-		assert_ok!(KittiesModule::do_something(Origin::signed(1), 42));
-		// Read pallet storage and assert an expected result.
-		assert_eq!(KittiesModule::something(), Some(42));
+		System::set_block_number(1);
+
+		// current total kitty
+		assert_eq!(KittiesModule::kitty_count(), 0);
+
+		// check create new kitty
+		assert_ok!(KittiesModule::create_kitty(Origin::signed(1)));
+		assert_eq!(KittiesModule::kitty_count(), 1);
+
+		// check kitty owned
+		let kitty_owned = KittiesModule::kitty_owned(1);
+		assert_eq!(kitty_owned.is_empty(), false);
+	});
+}
+
+#[test]
+fn test_transfer_kitty() {
+	new_test_ext().execute_with(|| {
+		System::set_block_number(1);
+
+		// create new kitty
+		// current total kitty
+		assert_eq!(KittiesModule::kitty_count(), 0);
+		// check create new kitty extrinsis
+		assert_ok!(KittiesModule::create_kitty(Origin::signed(1)));
+		assert_eq!(KittiesModule::kitty_count(), 1);
+		// check kitty owned
+		let mut kitty_owned_from = KittiesModule::kitty_owned(1);
+		assert_eq!(kitty_owned_from.is_empty(), false);
+
+		//transfer
+		// get kitty dna
+		let kitty_dna = *kitty_owned_from.last().unwrap();
+		let mut kitty_owned_to = KittiesModule::kitty_owned(2);
+		assert_eq!(kitty_owned_to.is_empty(), true);
+
+		// transfer extrinsis
+		assert_ok!(KittiesModule::transfer_kitty(Origin::signed(1), 2, kitty_dna));
+
+		// recheck kitty owned
+		kitty_owned_to = KittiesModule::kitty_owned(2);
+		kitty_owned_from = KittiesModule::kitty_owned(1);
+		assert_eq!(kitty_owned_from.is_empty(), true);
+		assert_eq!(kitty_owned_to.is_empty(), false);
 	});
 }
 
